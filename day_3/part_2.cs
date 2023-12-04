5233144bb995ebab73469bed100bea9cb9210bfd
justin <tenepid@gmail.com>
2023-12-04 13:41:07 -0800

    day 3 part 2 done

--- day_3/part_2.cs
+++ day_3/part_2.cs
@@ -9,77 +9,77 @@ namespace AoC.Day3
             for(int a = 0; a < grid.Length; a++)
                 for(int b = 0; b < grid[a].Length; b++)
                     if(grid[a][b] == '*')
-                        sum += GearRatio(x: b, y: a);
+                        sum += GearRatio(x: b, y: a, new HashSet<string>());
 
             return sum;
         }
 
-        private long GearRatio(int x, int y)
+        private long GearRatio(int x, int y, HashSet<string> avoid_repeats)
         {
             long sum = 1;
             int count = 0;
 
             //left
-            if(BoundsCheck(x - 1, y) && char.IsDigit(grid[y][x - 1]))
+            if(BoundsCheck(x - 1, y) && !avoid_repeats.Contains($"x:{x - 1}, y:{y}") && char.IsDigit(grid[y][x - 1]))
             {
                 count++;
-                sum *= NewDigitizer(x - 1, y);
+                sum *= NewDigitizer(x - 1, y, avoid_repeats);
             }
 
             //right
-            if(BoundsCheck(x + 1, y) && char.IsDigit(grid[y][x + 1]))
+            if(BoundsCheck(x + 1, y) && !avoid_repeats.Contains($"x:{x + 1}, y:{y}") && char.IsDigit(grid[y][x + 1]))
             {
                 count++;
-                sum *= NewDigitizer(x + 1, y);
+                sum *= NewDigitizer(x + 1, y, avoid_repeats);
             }
             
             //up
-            if(BoundsCheck(x, y - 1) && char.IsDigit(grid[y - 1][x]))
+            if(BoundsCheck(x, y - 1) && !avoid_repeats.Contains($"x:{x}, y:{y - 1}") && char.IsDigit(grid[y - 1][x]))
             {
                 count++;
-                sum *= NewDigitizer(x, y - 1);
+                sum *= NewDigitizer(x, y - 1, avoid_repeats);
             }
             
             //down
-            if(BoundsCheck(x, y + 1) && char.IsDigit(grid[y + 1][x]))
+            if(BoundsCheck(x, y + 1) && !avoid_repeats.Contains($"x:{x}, y:{y + 1}") && char.IsDigit(grid[y + 1][x]))
             {
                 count++;
-                sum *= NewDigitizer(x, y + 1);
+                sum *= NewDigitizer(x, y + 1, avoid_repeats);
             }
 
             //diagonal-up-left
-            if(BoundsCheck(x - 1, y - 1) && char.IsDigit(grid[y - 1][x - 1]))
+            if(BoundsCheck(x - 1, y - 1) && !avoid_repeats.Contains($"x:{x - 1}, y:{y - 1}") && char.IsDigit(grid[y - 1][x - 1]))
             {
                 count++;
-                sum *= NewDigitizer(x - 1, y - 1);
+                sum *= NewDigitizer(x - 1, y - 1, avoid_repeats);
             }
 
             //diagonal-up-right
-            if(BoundsCheck(x + 1, y - 1) && char.IsDigit(grid[y - 1][x + 1]))
+            if(BoundsCheck(x + 1, y - 1) && !avoid_repeats.Contains($"x:{x + 1}, y:{y - 1}") && char.IsDigit(grid[y - 1][x + 1]))
             {
                 count++;
-                sum *= NewDigitizer(x + 1, y - 1);
+                sum *= NewDigitizer(x + 1, y - 1, avoid_repeats);
             }
 
             //diagonal-down-left
-            if(BoundsCheck(x - 1, y + 1) && char.IsDigit(grid[y + 1][x - 1]))
+            if(BoundsCheck(x - 1, y + 1) && !avoid_repeats.Contains($"x:{x - 1}, y:{y + 1}") && char.IsDigit(grid[y + 1][x - 1]))
             {
                 count++;
-                sum *= NewDigitizer(x - 1, y + 1);
+                sum *= NewDigitizer(x - 1, y + 1, avoid_repeats);
             }
 
             //diagonal-down-right
-            if(BoundsCheck(x + 1, y + 1) && char.IsDigit(grid[y + 1][x + 1]))
+            if(BoundsCheck(x + 1, y + 1) && !avoid_repeats.Contains($"x:{x + 1}, y:{y + 1}") && char.IsDigit(grid[y + 1][x + 1]))
             {
                 count++;
-                sum *= NewDigitizer(x + 1, y + 1);
+                sum *= NewDigitizer(x + 1, y + 1, avoid_repeats);
             }
 
             if(count == 2) return sum;
             else return 0;
         }
 
-        private long NewDigitizer(int x, int y)
+        private long NewDigitizer(int x, int y, HashSet<string> bar)
         {
             int start = 0, end = x_max;
 
@@ -99,16 +99,13 @@ namespace AoC.Day3
             
             string temp = "";
             for(int c = start; c <= end; c++)
+            {
                 temp += grid[y][c];
+                bar.Add($"x:{c}, y:{y}");
+            }
             
 
             return int.Parse(temp);
-        }
-
-        public static void Main(string[] args)
-        {
-            var sol = new Day3();
-            Console.WriteLine(sol.GearRatioSum());
         }
     }
 }
