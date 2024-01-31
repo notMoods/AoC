f1bf4d0f74e85b8b3520ace01b3eb42ff0c94dc4
justin <tenepid@gmail.com>
2024-01-31 13:05:04 -0800

    h

---
+++ AoC.Puzzles/2023/day_12/day_12.cs
@@ -0,0 +1,15 @@
+namespace AoC.Puzzles.Y_2023
+{
+    class Day12 : Tools.IDay
+    {
+        public (string, string) Solution(string path)
+        {
+            var input = File.ReadAllLines(path);
+            long res1 = 0;
+
+
+            return (res1.ToString(), "0");
+        }
+        
+    }
+}
--- AoC.Puzzles/2023/day_12/part1.cs
+++
@@ -1,7 +0,0 @@
-namespace AoC.Puzzles.Y_2023
-{
-    partial class Day12
-    {
-        
-    }
-}
